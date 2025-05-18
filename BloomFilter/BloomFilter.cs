b1be9d374faf064ed1e2c2aaebe06a1641456418
VeerShahCodes <shahveer1213@gmail.com>
2025-05-18 11:27:34 -0700

    created bloom filter class

---
+++ BloomFilter/BloomFilter.cs
@@ -0,0 +1,65 @@
+﻿using System.Security.Principal;
+
+namespace BloomFilter
+{
+    public class BloomFilter<T>
+    {
+        bool[] booleans;
+        HashSet<Func<T, int>> hashSet;
+        public BloomFilter(int cap)
+        {
+            booleans = new bool[cap];
+            hashSet = new HashSet<Func<T, int>>();
+            hashSet.Add(HashFuncOne);
+            hashSet.Add(HashFuncTwo);
+            hashSet.Add(HashFuncThree);
+        }
+
+        public void LoadHashFunc(Func<T, int> hashFunc)
+        {
+            hashSet.Add(hashFunc);
+        }
+
+        public void Insert(T item)
+        {
+            int index = 0;
+            foreach(var func in hashSet)
+            {
+                index = func(item);
+            }
+            booleans[index] = true;
+        }
+
+        public bool ProbablyContains(T item)
+        {
+            int index = 0;
+            foreach (var func in hashSet)
+            {
+                index = func(item);
+            }
+            if (booleans[index] == false) return false;
+            return true;
+        }
+
+        private int HashFuncOne(T item)
+        {
+            return item.GetHashCode();
+        }
+
+        private int HashFuncTwo(T item)
+        {
+            string dummyString = "dummystring";
+            return (dummyString, item).GetHashCode();
+        }
+
+        private int HashFuncThree(T item)
+        {
+            int hash = 17;
+
+            hash *= (HashFuncOne(item), HashFuncTwo(item)).GetHashCode();
+
+            return hash;
+
+        }
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using BloomFilter;
+namespace UnitTest
+{
+    
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
