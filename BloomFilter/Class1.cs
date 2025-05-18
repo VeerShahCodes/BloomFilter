079ff62e525cebcc620e181f83dc69355b30b195
VeerShahCodes <shahveer1213@gmail.com>
2025-05-18 10:58:29 -0700

    Add project files.

---
+++ BloomFilter/Class1.cs
@@ -0,0 +1,41 @@
+﻿namespace BloomFilter
+{
+    public class BloomFilter<T>
+    {
+        bool[] booleans;
+        public BloomFilter(int cap)
+        {
+
+        }
+
+        public void LoadHashFunc(Func<T, int> hashFunc)
+        {
+
+        }
+
+        public void Insert(T item)
+        {
+
+        }
+
+        public bool ProbablyContains(T item)
+        {
+            return false;
+        }
+
+        private int HashFuncOne(T item)
+        {
+            return 0;
+        }
+
+        private int HashFuncTwo(T item)
+        {
+            return 0;
+        }
+
+        private int HashFuncThree(T item)
+        {
+            return 0;
+        }
+    }
+}
